92a852719cdd6e6b09176a80430dcd1c282891a6
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-06-19 13:29:59 +0200

    :hammer: Resolve aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
+const { readdirSync } = require('fs');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src', './resources'],
+        alias: {
+          '@resources': './resources',
+          ...readdirSync('./src', { withFileTypes: true })
+            .filter(dirent => dirent.isDirectory())
+            .map(dirent => dirent.name)
+            .reduce(
+              (res, item) => ({
+                ...res,
+                [`@/${item}`]: `./src/${item}`,
+              }),
+              {},
+            ),
+        },
+      },
+    ],
+  ],
 };
