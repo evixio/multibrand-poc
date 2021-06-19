1abd497c5106cab3e154f8e8d482cf853234833c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-06-19 13:23:49 +0200

    :rotating_light: Linting™

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, Text} from 'react-native';
+import { SafeAreaView, ScrollView, Text } from 'react-native';
 
 const App = () => {
   return (
