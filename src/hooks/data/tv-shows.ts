6980e8e23960934dea03d7bf6f1d512a07ff789d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-06-19 14:47:13 +0200

    :sparkles: usePopularShows

---
+++ src/hooks/data/tv-shows.ts
@@ -0,0 +1,44 @@
+import { useMemo } from 'react';
+import Config from 'react-native-config';
+import useSWR from 'swr';
+
+const API_URL = 'https://api.themoviedb.org';
+const API_VERSION = '3';
+const API_KEY = Config.TMDB_API_KEY;
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
+type TVShow = {
+  name: string;
+  description: string;
+  imageUrl: string;
+};
+
+export const usePopularShows = (): TVShow[] => {
+  const { data, error } = useSWR(
+    `${API_URL}/${API_VERSION}/tv/popular?api_key=${API_KEY}`,
+  );
+
+  const tvShows = useMemo(() => {
+    if (!data) {
+      return [];
+    }
+
+    if (error) {
+      return [];
+    }
+
+    if (data?.results?.length > 0) {
+      return data.results
+        .map((item: Record<string, unknown>) => ({
+          name: item.original_name,
+          description: item.overview || null,
+          imageUrl: item.poster_path ? `${IMAGE_URL}${item.poster_path}` : null,
+        }))
+        .filter((item: TVShow) => item.imageUrl && item.description);
+    }
+
+    return [];
+  }, [data, error]);
+
+  return tvShows;
+};
