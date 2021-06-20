e4e733b404ecbae79e64ee9f831aa09c31221dd4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-06-20 10:00:17 +0200

    :recycle: More generic project name

--- android/app/src/main/java/io/evix/multibrand/MainActivity.java
+++ android/app/src/main/java/io/evix/multibrand/MainActivity.java
@@ -1,4 +1,4 @@
-package com.telenet;
+package io.evix.multibrand;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Telenet";
+    return "Multibrand";
   }
 }
